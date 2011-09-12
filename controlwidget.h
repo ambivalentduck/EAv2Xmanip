15ac20c94a585e37aea80af2a6ad4b16eaac527a
Justin Horowitz <justin.horowitz@gmail.com>
2011-09-12 15:46:25 -0500

    Added rotationflip as stimulus option.

--- controlwidget.h
+++ controlwidget.h
@@ -45,7 +45,7 @@ private:
 	double * minJerkParams[6];
 	double viscousity,curl,saddle, inertia,KSpring, kickP, kickD, offsetMag, offsetTau;
 	enum treatments {UNTREATED=0, EA=1, X2=2} treatment;
-	enum stimuli {UNSTIMULATED=0, CURL=1, SADDLE=2, ROTATION45=3} stimulus;
+	enum stimuli {UNSTIMULATED=0, CURL=1, SADDLE=2, ROTATION45=3, ROTATIONFLIP=4} stimulus;
 	enum GameState {acquireTarget=0, inTarget=1} state;
 	std::vector<QWidget*> grayList;
 	std::vector<DisplayWidget::Sphere> sphereVec;
